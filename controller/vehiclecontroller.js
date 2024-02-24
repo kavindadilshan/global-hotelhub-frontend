a96c050d2fcd6e28cb3355b436a1ef43bd209afd
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-24 13:01:22 +0530

    created compre ui

--- controller/vehiclecontroller.js
+++ controller/vehiclecontroller.js
@@ -1,20 +1,20 @@
-
 function filterResult() {
 
     console.log(Cookies.get('token'));
 
     let location = document.getElementById("location").value.trim();
-    let brand = document.getElementById("brand").value.trim();
-    let condition = document.getElementById("condition").value.trim();
-    let fuelType = document.getElementById("fuelType").value.trim();
-    let year = document.getElementById("year").value.trim();
+    let checkin = document.getElementById("inputCheckIn").value.trim();
+    let checkout = document.getElementById("inputCheckOut").value.trim();
+    let adult = document.getElementById("adult").value.trim();
+    let child = document.getElementById("child").value.trim();
+    let room = document.getElementById("room").value.trim();
 
-    let token=Cookies.get('token');
-    let userId=Cookies.get('id');
+    let token = Cookies.get('token');
+    let userId = Cookies.get('id');
 
     let ajaxConfig = {
         method: "POST",
-        url: Server + "/v1/vehicle/filter",
+        url: Server + "/v1/hotels/filter",
         async: true,
         crossDomain: true,
         processData: false,
@@ -24,11 +24,14 @@ function filterResult() {
         },
         data: JSON.stringify({
             "userId": userId,
-            "location": location.toLowerCase(),
-            "brand": brand.toLowerCase(),
-            "condition": condition.toLowerCase(),
-            "fuelType": fuelType.toLowerCase(),
-            "year": year
+            "location": location,
+            "checkin": new Date(checkin).toISOString().split('T')[0].toString(),
+            "checkout": new Date(checkout).toISOString().split('T')[0].toString(),
+            "adults": adult.toString(),
+            "child": child.toString(),
+            "room": room.toString(),
+            "maxPrice": "50000",
+            "minPrice": "5000"
 
         })
     }
@@ -49,7 +52,7 @@ function filterResult() {
 
             swal.close();
 
-            if(response.body.length === 0){
+            if (response.body.length === 0) {
 
                 let x = "<div style='text-align: center'><h2>Results Not Found</h2></div>";
 
@@ -60,44 +63,49 @@ function filterResult() {
             for (let i = 0; i < response.body.length; i++) {
 
 
-               var x =" <li class=\"item col-lg-4 col-md-3 col-sm-4 col-xs-6\">\n" +
-                     "                                        <div class=\"item-inner\">\n" +
-                     "                                            <div class=\"item-img\">\n" +
-                     "                                                <div class=\"item-img-info\">\n" +
-                     "                                                    <img\n" +
-                     "                                                            src="+response.body[i].image+" alt=\"Retis lapen casen\">\n" +
-                     "                                                    <div class=\"item-box-hover\">\n" +
-                     "                                                    </div>\n" +
-                     "                                                </div>\n" +
-                     "                                            </div>\n" +
-                     "                                            <div class=\"item-info\">\n" +
-                     "                                                <div class=\"info-inner\">\n" +
-                     "                                                    <div class=\"item-title\">\n" +
-                     "\n" +
-                     "                                                        <a title=\"Retis lapen casen\">"+response.body[i].title+"</a></div>\n" +
-                     "                                                    <div class=\"item-content\">\n" +
-                     "                                                        <div class=\"item-price\">\n" +
-                     "                                                            <div class=\"price-box\"><span class=\"regular-price\"><span\n" +
-                     "                                                                    class=\"price\">"+response.body[i].price+"</span> </span></div>\n" +
-                     "                                                        </div>\n" +
-                     "                                                        <div class=\"other-info\">\n" +
-                     "                                                            <div class=\"col-km\"><i class=\"fa fa-location-arrow\"></i> "+response.body[i].address+"n" +
-                     "                                                            </div>\n" +
-                     "                                                            <div class=\"col-engine\"><i class=\"fa fa-calendar-times-o\"></i> "+response.body[i].dateTime+"\n" +
-                     "                                                            </div>\n" +
-                     "                                                            <div class=\"col-date\"><i class=\"fa fa-chrome\"\n" +
-                     "                                                                                     aria-hidden=\"true\"></i> "+response.body[i].publishSite+"\n" +
-                     "                                                            </div>\n" +
-                     "                                                        </div>\n" +
-                     "                                                    </div>\n" +
-                     "                                                </div>\n" +
-                     "                                            </div>\n" +
-                     "                                        </div>\n" +
-                     "                                    </li>";
+                var x = " <li class=\"item col-lg-4 col-md-3 col-sm-4 col-xs-6\">\n" +
+                    "                                        <div class=\"item-inner\">\n" +
+                    "                                            <div class=\"item-img\">\n" +
+                    "                                                <div class=\"item-img-info\">\n" +
+                    "                                                    <img\n" +
+                    "                                                            src=" + response.body[i].image + " alt=\"Retis lapen casen\">\n" +
+                    "                                                    <div class=\"item-box-hover\">\n" +
+                    "                                                    </div>\n" +
+                    "                                                </div>\n" +
+                    "                                            </div>\n" +
+                    "                                            <div class=\"item-info\">\n" +
+                    "                                                <div class=\"info-inner\">\n" +
+                    "                                                    <div class=\"item-title\">\n" +
+                    "\n" +
+                    "                                                        <a title=\"Retis lapen casen\">" + response.body[i].title + "</a></div>\n" +
+                    "                                                    <div class=\"item-content\">\n" +
+                    "                                                        <div class=\"item-price\">\n" +
+                    "                                                            <div class=\"price-box\"><span class=\"regular-price\"><span\n" +
+                    "                                                                    class=\"price\">" + response.body[i].price + "</span> </span></div>\n" +
+                    "                                                        </div>\n" +
+                    "                                                        <div class=\"other-info\">\n" +
+                    "                                                        <div class=\"row-container\">\n" +
+                    "                                                            <div class=\"col-km\"><i class=\"fa fa-location-arrow\"></i></div> <div class=\"col-km\">" + response.body[i].address + "n" +
+                    "                                                            </div>\n" +
+                    "                                                            </div>\n" +
+                    "                                                        <div class=\"row-container\">\n" +
+                    "                                                            <div class=\"col-km\"><i class=\"fa fa-calendar-times-o\"></i></div> <div class=\"col-km\"> " + response.body[i].amenities + "\n" +
+                    "                                                            </div>\n" +
+                    "                                                            </div>\n" +
+                    "                                                        <div class=\"row-container\">\n" +
+                    "                                                            <div class=\"col-km\"><i class=\"fa fa-chrome\"\n" +
+                    "                                                                                     aria-hidden=\"true\"></i> </div><div class=\"col-km\">" + response.body[i].publishSite + "\n" +
+                    "                                                            </div>\n" +
+                    "                                                            </div>\n" +
+                    "                                                        </div>\n" +
+                    "                                                    </div>\n" +
+                    "                                                </div>\n" +
+                    "                                            </div>\n" +
+                    "                                        </div>\n" +
+                    "                                    </li>";
 
 
                 $('#grid').append(x);
-
             }
 
         }
